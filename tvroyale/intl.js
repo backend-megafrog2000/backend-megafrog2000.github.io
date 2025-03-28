84d94300369956003cb1a4a30586d44e7882598a
megafrog2000 <megafrog2000@protonmail.com>
2025-03-27 20:54:21 -0400

    Add files via upload

---
+++ tvroyale/intl.js
@@ -0,0 +1,30 @@
+function checkupdate() {
+    needupdate = 0;
+    return needupdate;
+}
+function geturl() {
+    url = ""
+    return url
+}
+function getchannels() {
+    let channeldb = [
+        ["NULL", "NULL"],
+        ["../index.html", "https://backend-megafrog2000.github.io/tvroyale/banners/legacy/modern.png"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"]
+    ];
+    return channeldb;
+}
---
+++ tvroyale/legacy.js
@@ -0,0 +1,31 @@
+function checkupdate() {
+    needupdate = 0;
+    return needupdate;
+}
+function geturl() {
+    url = ""
+    return url
+}
+function getchannels() {
+    let channeldb = [
+        ["NULL", "NULL"],
+        ["https://turnerlive.warnermediacdn.com/hls/live/2023168/tnteast/slate/VIDEO_0_3564000.m3u8", "https://backend-megafrog2000.github.io/tvroyale/banners/legacy/tnt.svg"],
+        ["https://a-cdn.herringnetwork.com/affiliate/awee/playlist.m3u8", "https://backend-megafrog2000.github.io/tvroyale/banners/legacy/awe.svg"],
+        ["http://fl2.moveonjoy.com/PARAMOUNT_NETWORK/index.m3u8", "https://backend-megafrog2000.github.io/tvroyale/banners/legacy/paramount.svg"],
+        ["http://fl2.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-megafrog2000.github.io/tvroyale/banners/legacy/newsnation.svg"],
+        ["https://turnerlive.warnermediacdn.com/hls/live/2023183/aseast/noslate/VIDEO_1_5128000.m3u8", "https://backend-megafrog2000.github.io/tvroyale/banners/legacy/adultswim.svg"],
+        ["../index.html", "https://backend-megafrog2000.github.io/tvroyale/banners/legacy/modern.png"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"]
+    ];
+    return channeldb;
+}
