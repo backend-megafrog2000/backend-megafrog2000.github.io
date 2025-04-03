b20bc5ca90dd8e56c3ae22442c9e05739ca0df7f
megafrog2000 <megafrog2000@protonmail.com>
2025-04-03 14:53:27 -0400

    Add files via upload

---
+++ tvroyale/v0/back.js
@@ -0,0 +1,34 @@
+function checkupdate() {
+    needupdate = 0;
+    return needupdate;
+}
+function geturl() {
+    url = ""
+    return url
+}
+function getchannels() {
+    let channeldb = [
+        ["NULL", "NULL"],
+        ["http://api.toonamiaftermath.com:3000/est/playlist.m3u8", "https://backend-megafrog2000.github.io/tvroyale/v0/bannners/toonami.png"],
+        ["http://v1.weatherscan.net", "https://backend-megafrog2000.github.io/tvroyale/v0/bannners/weatherscan.png"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"]
+    ];
+    return channeldb;
+}
