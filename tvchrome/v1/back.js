f1d113a23750d74bc94c7057f6fd262fecd34908
megafrog2000 <megafrog2000@protonmail.com>
2025-04-08 14:04:23 -0400

    Add files via upload

--- tvchrome/v1/back.js
+++ tvchrome/v1/back.js
@@ -2,16 +2,16 @@ function getchannels() {
     channels = [
         ["https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8", "nochan.gif"],
         ["NULL", "NULL"],
-        ["http://cfd-v4-service-channel-stitcher-use1-1.prd.pluto.tv/stitch/hls/channel/62ba60f059624e000781c436/master.m3u8?appName=web&appVersion=unknown&clientTime=0&deviceDNT=0&deviceId=6c25e430-30d3-11ef-9cf5-e9ddff8ff496&deviceMake=Chrome&deviceModel=web&deviceType=web&deviceVersion=unknown&includeExtendedEvents=false&serverSideAds=false&sid=1b7de8e4-d114-4438-b098-6f7aee77b4be", "https://megafrog2000.github.io/tvchrome/v1/ch2.gif"],
-        ["https://v1.weatherscan.net/", "https://megafrog2000.github.io/tvchrome/v1/ch3.gif"],
-        ["http://fl2.moveonjoy.com/NEWS_NATION/index.m3u8", "https://megafrog2000.github.io/tvchrome/v1/ch4.gif"],
-        ["http://api.toonamiaftermath.com:3000/est/playlist.m3u8", "https://megafrog2000.github.io/tvchrome/v1/ch5.gif"],
-        ["http://cfd-v4-service-channel-stitcher-use1-1.prd.pluto.tv/stitch/hls/channel/5fd7bca3e0a4ee0007a38e8c/master.m3u8?appName=web&appVersion=unknown&clientTime=0&deviceDNT=0&deviceId=6c2cc208-30d3-11ef-9cf5-e9ddff8ff496&deviceMake=Chrome&deviceModel=web&deviceType=web&deviceVersion=unknown&includeExtendedEvents=false&serverSideAds=false&sid=9c78a281-5d61-4850-8b62-74632f015343", "https://megafrog2000.github.io/tvchrome/v1/ch6.gif"],
-        ["https://master.nhkworld.jp/nhkworld-tv/playlist/live.m3u8", "https://megafrog2000.github.io/tvchrome/v1/ch7.gif"],
-        ["https://ntv4.mov3.co/hls/ntv.m3u8", "https://megafrog2000.github.io/tvchrome/v1/ch8.gif"],
-        ["http://vthanh.utako.moe/TV_Asahi/index.m3u8", "https://megafrog2000.github.io/tvchrome/v1/ch9.gif"],
-        ["https://tbs5.mov3.co/hls/tbs.m3u8", "https://megafrog2000.github.io/tvchrome/v1/ch10.gif"],
-        ["http://vthanh.utako.moe/Fuji_TV/index.m3u8", "https://megafrog2000.github.io/tvchrome/v1/ch11.gif"]
+        ["http://cfd-v4-service-channel-stitcher-use1-1.prd.pluto.tv/stitch/hls/channel/62ba60f059624e000781c436/master.m3u8?appName=web&appVersion=unknown&clientTime=0&deviceDNT=0&deviceId=6c25e430-30d3-11ef-9cf5-e9ddff8ff496&deviceMake=Chrome&deviceModel=web&deviceType=web&deviceVersion=unknown&includeExtendedEvents=false&serverSideAds=false&sid=1b7de8e4-d114-4438-b098-6f7aee77b4be", "https://backend-megafrog2000.github.io/tvchrome/v1/ch2.gif"],
+        ["https://v1.weatherscan.net/", "https://backend-megafrog2000.github.io/tvchrome/v1/ch3.gif"],
+        ["http://fl2.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-megafrog2000.github.io/tvchrome/v1/ch4.gif"],
+        ["http://api.toonamiaftermath.com:3000/est/playlist.m3u8", "https://backend-megafrog2000.github.io/tvchrome/v1/ch5.gif"],
+        ["http://cfd-v4-service-channel-stitcher-use1-1.prd.pluto.tv/stitch/hls/channel/5fd7bca3e0a4ee0007a38e8c/master.m3u8?appName=web&appVersion=unknown&clientTime=0&deviceDNT=0&deviceId=6c2cc208-30d3-11ef-9cf5-e9ddff8ff496&deviceMake=Chrome&deviceModel=web&deviceType=web&deviceVersion=unknown&includeExtendedEvents=false&serverSideAds=false&sid=9c78a281-5d61-4850-8b62-74632f015343", "https://backend-megafrog2000.github.io/tvchrome/v1/ch6.gif"],
+        ["https://master.nhkworld.jp/nhkworld-tv/playlist/live.m3u8", "https://backend-megafrog2000.github.io/tvchrome/v1/ch7.gif"],
+        ["https://ntv4.mov3.co/hls/ntv.m3u8", "https://backend-megafrog2000.github.io/tvchrome/v1/ch8.gif"],
+        ["http://vthanh.utako.moe/TV_Asahi/index.m3u8", "https://backend-megafrog2000.github.io/tvchrome/v1/ch9.gif"],
+        ["https://tbs5.mov3.co/hls/tbs.m3u8", "https://backend-megafrog2000.github.io/tvchrome/v1/ch10.gif"],
+        ["http://vthanh.utako.moe/Fuji_TV/index.m3u8", "https://backend-megafrog2000.github.io/tvchrome/v1/ch11.gif"]
     ];
     return channels;
 }
