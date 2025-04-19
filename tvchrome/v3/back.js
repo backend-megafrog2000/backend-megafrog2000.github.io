983cfa4e8138790474fb72ccc44c12506fdbcc22
megafrog2000 <megafrog2000@protonmail.com>
2025-04-18 22:17:34 -0400

    Add files via upload

--- tvchrome/v3/back.js
+++ tvchrome/v3/back.js
@@ -1,1 +1,27 @@
-//add code here
+function getchannels() {
+    channels = [
+        ["https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8", "nochan.gif"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"]
+    ];
+    return channels;
+}
